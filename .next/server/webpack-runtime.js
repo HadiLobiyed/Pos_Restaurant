69be9d5d64ffd236b203ad928ee76613a3780151
MSI1 <hadigaming31@gmail.com>
2026-03-19 08:17:53 +0100

    Remove prisma.config.ts to fix Vercel build

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b3e0adab45e41a54")
+/******/ 		__webpack_require__.h = () => ("718232fb2f89fc28")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16545b7fd24f7244"; }
+/******/ 		__webpack_require__.h = function() { return "65fd1fda22094736"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
